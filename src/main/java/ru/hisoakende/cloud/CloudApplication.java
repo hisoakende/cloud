676a59e81040384dddb83e2b6c33c4320adca256
hisoakende <hisoakende@gmail.com>
2023-09-20 12:56:10 +0300

    init commit

---
+++ src/main/java/ru/hisoakende/cloud/CloudApplication.java
@@ -0,0 +1,13 @@
+package ru.hisoakende.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
+
+}
